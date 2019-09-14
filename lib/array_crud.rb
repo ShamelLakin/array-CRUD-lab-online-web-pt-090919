6b27914bbc912b8e16043ec9861a5e3362de087a
Shamel Lakin <christlife4u@gmail.com>
2019-09-14 21:09:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,13 +24,14 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  new_array[2]
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+  new_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  new_array[-1]
 end
